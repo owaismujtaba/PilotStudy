f93764c5db668737e62c8e803137a15ff3b328a6
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2024-09-19 17:48:42 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
 import os
 from pathlib import Path
 
+# Functionality
+createBIDSFile = False
+loadData = True
 
-createBIDSFile = True
-loadData = False
-
+# Directories
 currDir = os.getcwd()
 rawDataDir = Path(currDir, 'RawData', 'EEG')
 bidsDir = Path(currDir, 'BIDS')
+dataDir = Path(currDir, 'Data')
 
+
+tmax = 1.0
+tmin = 0.0
