c7f2857a0106bbd5ae27cc6f51068dc1f578ee4c
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2024-10-22 14:33:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,98 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from pathlib import Path
+from colorama import Fore, Style, init
+
+from src.models.models import RandomForestModel,  EEGNet
+from src.models.trainer import ModelTrainer
+from src.dataset.data_extractor import VowelDataExtractor
+import src.utils.config as config
+from src.utils.utils import printSectionFooter, printSectionHeader
+from src.utils.utils import normalize_data
+
 import pdb
-from src.bids_dataset import XDFData
-import src.config as config
-from src.data_extractor import extractWordSyllableDataForAllSubjects
-from src.dataset_loader import VowelDataset
-from src.models import RandomForestModel
-from src.trainer import ModelTrainer
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
 
-filepath='C:\\DeepRESTORE\\PilotStudy\\RawData\\sub-P001\\ses-S001\\eeg\\sub-P001_ses-S001_task-Default_run-001_eeg.xdf'
 
-subjectId = '01'
-sessionId = '01'
-runId = '01'
+
+init(autoreset=True)
+
+if config.extractEpochsData:
+    from src.dataset.data_extractor import extractEpochsDataForAllSubjects
+    extractEpochsDataForAllSubjects()
+
+if config.createBIDSFile:
+    from src.dataset.bids_dataset import createBIDSDataset
+    filePath = '/home/owaismujtaba/projects/PilotStudy/files.csv'
+    createBIDSDataset(csvFilePath=filePath)
+
+
+if config.visualization:
+    from src.utils.visualization import plotVowelActivityAllSubjects
+    from src.dataset.data_extractor import VowelDataExtractor
+    from src.utils.visualization import plotRealSilentAverageActivityAllSubjects
+
+    plotRealSilentAverageActivityAllSubjects()
+    #VowelDataExtractor(subjectId='01', sessionId='01')=
+
+    #plotVowelActivityAllSubjects()
 
 
 if config.trainModels:
-    print('\n' + '=' * 60)
-    print('🚀  Starting Model Training Process  🚀'.center(60))
-    print('=' * 60)
+    from src.models.models import DenseModel
+    from src.dataset.data_extractor import VowelDataExtractor
+    printSectionHeader('  Starting Model Training Process  '.center(60))
 
-    vowelDatasetExtractor = VowelDataset(
-        rootDir=config.dataDir,
-        subjectId=subjectId,
-        sessionId=sessionId,
-        speechType='Silent',
-        languageElement='Experiment',
-        eventType='Start',
-        trialPhase=None,
-        presentationMode='Speech'
-    )
-    data, labels = vowelDatasetExtractor.vowelData
-    data = data[:,:,500:]
+    subjectId = '01'
+    sessionId = '01'
     
-    # Reshape data for PCA
-    nSamples, nChannels, nTimepoints = data.shape
-    data_reshaped = data.reshape(nSamples, -1)
+    speechType= config.SPEECH_TYPE
+    languageElement=config.LANGUAGE_ELEMENT
+    eventType=config.EVENT_TYPE
+    trialPhase=config.TRIAL_PHASE
+    presentationMode=config.PRESENTATION_MODE
     
-    # Apply StandardScaler
-    print("Applying StandardScaler")
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data_reshaped)
     
-    # Apply PCA
-    print("Applying PCA")
-    pca = PCA(n_components=0.99, svd_solver='full')
-    dataPca = pca.fit_transform(data_scaled)
+    dataExtractor = VowelDataExtractor(subjectId, sessionId)
+    data = dataExtractor.data
+    labels = dataExtractor.labels
     
-    print(f"Original shape: {data.shape}, PCA shape: {dataPca.shape}")
-    print(f"Number of components: {pca.n_components_}")
+    print(f"{Fore.YELLOW}1️⃣ Applying zscore normalization...{Style.RESET_ALL}")
     
-    model = RandomForestModel()
-    trainer = ModelTrainer()
-    trainer.trainModel(model, dataPca, labels)
+    dataNormalized = normalize_data(data)
+       
+    nSamples, channels, nTimepoints = data.shape
+        
+        
+
+    '''
+        print(f"{Fore.YELLOW}2️⃣ Applying PCA...{Style.RESET_ALL}")   
+        pca = PCA(n_components=0.99, svd_solver='full')
+        dataPca = pca.fit_transform(data_scaled)
+        
+    '''
+        #printSectionHeader(f"Original shape: {dataScaled.shape}")
+    name = f'sub-{subjectId}_ses-{sessionId}'
     
+    destination = Path(
+            config.RESULTS_DIR, 
+            f'sub-{subjectId}', f'ses-{sessionId}',
+            f'{speechType}{languageElement}{eventType}{trialPhase}{presentationMode}'
+        )
+    
+    os.makedirs(destination, exist_ok=True)
+
+    data = np.mean(data, axis=1)
+    #model = EEGNet(num_classes=5, channels=data.shape[1], timestamps=data.shape[2])
+    model = DenseModel()
+    trainer = ModelTrainer(name=name, destination=destination)
+    labels = np.array(labels)
+    trainer.trainModel(model, data, labels)
+        
     print('\n' + '*' * 60)
     print('✅  Model Training Process Complete  ✅'.center(60))
     print('*' * 60 + '\n')
+
+
     
-    pdb.set_trace()
-
-
-
-if config.extractSyllableWordData:
-    print('\n' + '=' * 60)
-    print('🔄  Starting Syllable and Word Data Extraction  🔄'.center(60))
-    print('=' * 60)
-
-    extractWordSyllableDataForAllSubjects(
-        speechType='Silent',
-        languageElement='Experiment',
-        eventType='Start',
-        trialPhase=None,
-        presentationMode='Speech'
-    )
-
-    print('\n' + '*' * 60)
-    print('✅  Syllable and Word Data Extraction Complete  ✅'.center(60))
-    print('*' * 60 + '\n')
-
-if config.createBIDSFile:
-    print('\n' + '=' * 60)
-    print('📂  Starting BIDS File Creation  📂'.center(60))
-    print('=' * 60)
-    print('\n' + '-' * 60)
-    print('📄  XDFData Details  📄'.center(60))
-    print('-' * 60)
-    print(f"File Path: {data.filePath}")
-    print(f"Subject ID: {data.subjectId}")
-    print(f"Session ID: {data.sessionId}")
-    print(f"Run ID: {data.runId}")
-    print('-' * 60 + '\n')
-    data = XDFData(
-        filePath=filepath,
-        subjectId=subjectId,
-        sessionId=sessionId,
-        runId=runId
-    )
-
-    print('\n' + '*' * 60)
-    print('✅  BIDS File Creation Complete  ✅'.center(60))
-    print('*' * 60 + '\n')
-
